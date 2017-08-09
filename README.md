fcb74cc3659839be4362bc1252f6aaaf7170585b
Don Guckenberger <dguck23@gmail.com>
2017-08-09 12:32:04 -0600

    updated with continue function and updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# donazon
+WELCOME TO DONAZON
+--------------------------------------------------
+HOW TO USE DONAZON's CUSTOMER APP
+
+Launch donazonCustomer.js and you'll get a list of the items we have for sale. Not everything is in stock, however. To order, simply enter the product number and the quantity you require. We'll check our shelves and process your order if we have enough items available. 
+
+Get your credit card ready, though, because once you start making orders, you won't be able to stop!
+
+
+--------------------------------------------------
+HOW TO USE DONAZON's MANAGER APP
+
+
+--------------------------------------------------
+HOW TO USE DONAZON's SUPERVISOR APP
--- donazonCustomer.js
+++ donazonCustomer.js
@@ -14,6 +14,7 @@ var connection = mysql.createConnection ({
 //Let's say hello to our new customer
 console.log("Welcome to Donazon, the world's largest and greatest imaginary store. Take a look at our available products.");
 
+//We need to show the customer our items for sale.
 function loadProducts() {
 
 
@@ -31,8 +32,7 @@ ID: ${res[i].item_id} | NAME: ${res[i].item_name} | PRICE: $${res[i].price}
 		})
 };
 
-
-
+//What does our customer want to order? How many?
 function productOrder() {
 Inquirer.prompt([
 	{
@@ -66,7 +66,7 @@ Inquirer.prompt([
 	})
 
 }
-
+//We'll check the query of our current database and see if wee have enough items.
 function checkInventory(answer) {
 	var query = connection.query(
 
@@ -89,16 +89,13 @@ function checkInventory(answer) {
 				adjustInventory(answer, newQuantity, res[0].price);
 			} else {
 				console.log("Sorry, we only have " + res[0].stock_quantity + " items in stock.")
-				loadProducts();
+				continueShopping();
 			}
-
 		}
 		);	
 }
-
+//This function will reset the quantity of the product with our newQuantity value.
 function adjustInventory(answer, newQuantity, price) {
-	console.log(answer);
-	console.log(newQuantity);
 
 	var query = connection.query(
 
@@ -124,12 +121,33 @@ function adjustInventory(answer, newQuantity, price) {
 		}
 		);	
 }
-
+//We'll check the price of each item and calculate the total amount for customers to pay
 function createBill(answer, price) {
 	console.log("You ordered " + answer.quantity + " items.")
 	console.log("Let's add up your bill!");
 	var total = price * answer.quantity;
 	console.log("You owe us $" + total+ "! Pay up!" );
+	continueShopping();
+}
+
+//Does the customer want to keep shopping?
+function continueShopping() {
+	Inquirer.prompt([
+	{
+		type: "list",
+		message: "Do you want to continue shopping?",
+		choices: ["Yes, you have so many awesome things!", "No, I've already maxed out my credit card."],
+		name: "continue"
+	}
+		]).then(function(answer) {
+		// 
+		if (answer.continue === "Yes, you have so many awesome things!") {
+			loadProducts();
+		} else {
+			connection.end();
+		}
+
+	})
 }
 
 loadProducts();
